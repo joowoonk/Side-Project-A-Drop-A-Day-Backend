0a731b92142f86b97e63e56c2cfa737b7f3c2585
Golden Eagle <joowoon.kang@biola.edu>
2020-06-07 20:32:53 -0700

    added server able to run api from basic, will add other endpoints

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+// const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-rotuer");
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/api/auth", authRouter);
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
+});
+
+module.exports = server;
