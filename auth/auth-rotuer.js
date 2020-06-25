43d3ff91439c6d948fbcad0b376252d8cd0a5119
Golden Eagle <joowoon.kang@biola.edu>
2020-06-24 20:22:23 -0700

    login failed

--- auth/auth-rotuer.js
+++ auth/auth-rotuer.js
@@ -67,7 +67,7 @@ router.post("/register", (req, res) => {
 router.post("/login", userValidation, (req, res) => {
   const { username, password } = req.body;
   db("users")
-    .select("users.username", "users.id")
+    .select("*")
     .where({ username })
     .then(([user]) => {
       if (user && bcryptjs.compareSync(password, user.password)) {
