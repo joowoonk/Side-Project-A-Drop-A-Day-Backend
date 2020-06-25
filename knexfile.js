42c705399e7a59af5efe8bbc258bf3c5abf7466d
Golden Eagle <joowoon.kang@biola.edu>
2020-06-25 13:26:33 -0700

    updated knexfile.js, made slot for production

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,60 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+// const pgConnection =
+//   process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+
+// module.exports = {
+//   development: {
+//     client: "sqlite3",
+//     useNullAsDefault: true,
+//     connection: {
+//       filename: "./database/auth.db3",
+//     },
+//     pool: {
+//       afterCreate: (conn, done) => {
+//         conn.run("PRAGMA foreign_skeys = ON", done);
+//       },
+//     },
+//     migrations: {
+//       directory: "./database/migrations",
+//     },
+//     seeds: {
+//       directory: "./database/seeds",
+//     },
+//   },
+//   testing: {
+//     client: "sqlite3",
+//     connection: {
+//       filename: "./database/test.db3",
+//     },
+//     useNullAsDefault: true,
+//     migrations: {
+//       directory: "./database/migrations",
+//     },
+//     seeds: {
+//       directory: "./database/seeds",
+//     },
+//   },
+// };
 
 const pgConnection =
   process.env.DATABASE_URL || "postgresql://postgres@localhost/auth";
+// {
+// database: 'nodelogin', //postgres by default
+// user: 'postgres', //postgres by default
+// password: 'hinata5185', //blank by default
+// }
 
 module.exports = {
   development: {
     client: "sqlite3",
-    useNullAsDefault: true,
     connection: {
       filename: "./database/auth.db3",
     },
+    useNullAsDefault: true,
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_skeys = ON", done);
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
     migrations: {
@@ -28,6 +70,22 @@ module.exports = {
       filename: "./database/test.db3",
     },
     useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+  production: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/auth.db3",
+    },
+    pool: {
+      min: 2,
+      max: 2000,
+    },
     migrations: {
       directory: "./database/migrations",
     },
