7df13906a4d12a5bb1137c7389e39fa451dc9d65
Your Name <joowoonk@gmail.com>
2021-03-17 11:08:59 -0700

    trying to fix ssl issue again

--- knexfile.js
+++ knexfile.js
@@ -79,10 +79,12 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {connectionString: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL, 
     ssl: {
       rejectUnauthorized: false
-    },},
+    },
+  },
     pool: {
       min: 2,
       max: 10,
